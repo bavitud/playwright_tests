4a7cac2771b09e3851f7d52c2c3e75babd29ef6c
Brenda Avitud <brenda_avitud@epam.com>
2026-06-09 19:31:53 -0600

    testAdd

--- tests/example1.spec.js
+++ tests/example1.spec.js
@@ -18,4 +18,14 @@ test.describe('My First Test Suite', () => {
    test('console log', async ({ page }) => {
     await console.log ('test');
   });
+
+   test('console log', async ({ page }) => {
+    await console.log ('test1');
+  });
+
+   test('console log', async ({ page }) => {
+    await console.log ('test2');
+  test('console log', async ({ page }) => {
+    await console.log ('newtest');
+  });
 })
